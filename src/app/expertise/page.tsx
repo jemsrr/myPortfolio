ddcd944d7b9161fbc161a210d8b4ff5067e1c864
jemsrr <jemsrakholiya4949@gmail.com>
2024-06-08 16:14:25 +0530

    Add scroll

--- src/app/expertise/page.tsx
+++ src/app/expertise/page.tsx
@@ -9,6 +9,9 @@ import { TbBrandNextjs, TbBrandReact, TbBrandRedux } from "react-icons/tb";
 const Expertise = () => {
   return (
     <div className="min-h-screen px-[5%] sm:px-[10%] grid justify-items-center">
+      <div className=" bg-no-repeat bg-cover bg-[url('../../public/e1.png')] md:bg-[url('../../public/e2.png')] lg:bg-[url('../../public/e3.png')] fixed h-screen w-full -z-20 opacity-50">
+        <div className="w-full h-screen bg-slate-900 bg-opacity-20"></div>
+      </div>
       <div className="h-screen text-slate-500 font-extrabold text-5xl sm:text-9xl grid place-content-center justify-items-center animate-[fadeUp_2s_ease-out]">
         Expertise
         <FaAngleDown
@@ -18,14 +21,14 @@ const Expertise = () => {
           className=" text-slate-300 text-4xl font-extrabold absolute bottom-[20%] animate-bounce"
         />
       </div>
-      <div className="border-solid w-[90%] md:w-[80%] border-slate-800 border-2 pb-20 ">
+      <div className="border-solid bg-slate-900 bg-opacity-80 w-[90%] md:w-[80%] border-slate-800 border-2 my-20 ">
         <h1 className="text-3xl text-white font-bold w-full border-solid border-slate-800 border-y-2 py-6 px-2 sm:px-10">
           Coding Skills
         </h1>
         <div className="text-2xl flex justify-center text-white font-medium w-full border-solid border-slate-800 border-y-2 py-6 px-2 sm:px-10">
           Web Development
         </div>
-        <div className="font-black text-9xl grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 place-content-center justify-items-center py-10">
+        <div className="font-black text-7xl md:text-8xl lg:text-9xl grid grid-cols-2 sm:grid-cols-4 gap-6 place-content-center justify-items-center py-10">
           <FaHtml5 className="text-[#e34c26]" />
           <FaCss3Alt className="text-[#264de4]" />
           <SiJavascript className="text-[#f0db4f]" />
@@ -38,7 +41,7 @@ const Expertise = () => {
         <div className="text-2xl flex justify-center text-white font-medium w-full border-solid border-slate-800 border-y-2 py-6 px-2 sm:px-10">
           coding language
         </div>
-        <div className="font-black text-9xl grid sm:grid-cols-2 md:grid-cols-3 gap-10 place-content-center justify-items-center py-10">
+        <div className="font-black text-7xl md:text-8xl lg:text-9xl grid grid-cols-2 sm:grid-cols-3 gap-6 place-content-center justify-items-center py-10">
           <FaC className="text-[#264de4]" />
           <FaJava className="text-[#e34c26]" />
           <FaPython className="text-[#f0db4f]" />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`${league_Spartan.className} bg-slate-900`}>
         <AnimatedCursor
           color="0, 255, 255"
